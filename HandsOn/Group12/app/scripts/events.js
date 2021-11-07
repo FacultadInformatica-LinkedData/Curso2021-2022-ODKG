23529aa17c91231dded9fdcafe19c3b57ddbdb04
Alvaro Ramirez <alvaroame@hotmail.com>
2021-11-07 09:05:16 -0600

    New template for the app

--- HandsOn/Group12/app/scripts/events.js
+++ HandsOn/Group12/app/scripts/events.js
@@ -31,8 +31,11 @@ var queryEvents = `
     } LIMIT 10
     `
 var config = {
-        "selector": "#ResCol"
+        "selector": "#result"
       }
+
+var list_events = [];
+var number_events = 2;
 
 function loadEvents() {
     //this is dummy example
@@ -46,36 +49,146 @@ function loadEvents() {
       console.log(data)
     })
 
-    //This is dummy data json parsed reponse with 10 events
     console.log(jsonResponseExample);
-    jsonParsed = sparql.parseResponse(jsonResponseExample)
-    toEvents(jsonParsed, config)
-
+    list_events = sparql.parseResponse(jsonResponseExample)
+    loadMoreEvents(true) //render the first (number_events) events in HTML, deleting the previous
 }
 
-function toEvents(jsonParsed){
-   /* <div class="row" id="cardrow">
-     <div className="card" style="width: 18rem;">
-        <div className="card-body">
-            <h5 className="card-title">Event title</h5>
-            <p className="card-text">Date</p>
-            <p className="card-text">Facility</p>
-            <p className="card-text">Price</p>
-            <a href="#" className="btn btn-primary">Event page</a>
+function loadMoreEvents(reset=false) {
+    events = list_events.slice(0, number_events) //get a sublist of the first n(number_events) elements of events
+    renderEvents(events, config, reset)//render the events in HTML
+    list_events.splice(0,number_events)//delete the fist n(number_events) elements of events from original list
+}
+
+/*
+Render the list of events in HTML similar to the template
+if reset is true then previous events in DOM are deleted
+ */
+function renderEvents(events_json, config, reset=false){
+   /* This is the template created dinamicaly
+   <div class="card-body">
+        <h5 class="card-title">Las palabras desnudas</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Fortificación y ciudad. 29 maquetas</h6>
+        <p class="card-text"><small>Un hombre vive sumido en unaprofunda crisis tras varios añosseparado de la que fue su parejasentimental. La culpa, los remordimientosy la nostalgia, le mantienensumido en un estado de ansiedadconstante que le llevará a rozar lalocura. De ese período oscuronace la necesidad de escribir undiario personal. Lo que comenzócomo un ejercicio catártico, poco apoco se va convirtiendo en laaventura más fascinante de suvida? La que le llevará a encontrarun amor que jamás había conocido. 60 minutos</small></p>
+        <span class="badge bg-success">CursosTalleres</span>
+        <span class="badge bg-success">Mujeres</span>
+        <hr class="col-12">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><i class="bi bi-calendar-range-fill" title="Date range"></i> 2021-10-05 - 2021-10-06 </li>
+            <li class="list-group-item"><i class="bi bi-calendar-x-fill" title="Excluded days"></i> 12/10/2021</li>
+            <li class="list-group-item"><i class="bi bi-calendar-week-fill" title="Days"></i> M,T,W,S,F</li>
+            <li class="list-group-item"><i class="bi bi-clock-fill" title="Hour"></i> 20:00:00</li>
+            <li class="list-group-item"><i class="bi bi-geo-alt-fill" title="Held in"></i> Centro cultural</li>
+        </ul>
+        <hr class="col-12">
+
+        <i class="bi bi-currency-euro" title="Price"></i>
+        <small class="text-muted">Gratuito</small>
+
+        <div class="card-footer text-center">
+           <a class="btn btn-primary" href="http://www.madrid.es/sites/v/index.jsp?vgnextchannel=ca9671ee4a9eb410VgnVCM100000171f5a0aRCRD&amp;vgnextoid=785be0464605c710VgnVCM1000001d4a900aRCRD">Ver más</a>
         </div>
-    </div>*/
+    </div>
+   * */
     var opts = {
         "selector": config.selector || null
     }
 
-    var eventos = d3.select(opts.selector, "div")
-    eventoCard = eventos.append("div").attr("class", "row").attr("id", "cardrow")
-    eventoCard.selectAll("div")
-        .data(jsonParsed)
+    if(reset){
+        d3.select(opts.selector).selectAll("*").remove();
+    }
+
+    var eventosDiv = d3.select(opts.selector, "div")
+    var eventosRow = eventosDiv.append("div").attr("class", "row")
+    var events = eventosRow.selectAll(".card-body")
+        .data(events_json)
         .enter()
-        .append("div").attr("class", "card").attr("style", "width: 18rem")
+        .append("div").attr("class", "col col-xl-6 col-lg-12 col-md-12 col-sm-12")
+        .append("div").attr("class", "card")
         .append("div").attr("class", "card-body")
-        .append("h5").attr("class", "card-title").text(function(col) { return col.Label })
+
+    events.append("h5").attr("class", "card-title")
+        .text(function(col) { return col.Label })
+    events.append("h6").attr("class", "card-subtitle mb-2 text-muted")
+        .text(function(col) { return col.Title })
+    events.append("p").attr("class", "card-text")
+        .style("height", "30vh").style("overflow", "auto")
+        .append("small")
+        .text(function(col) { return col.Description })
+
+    events.append("span").attr("class", "badge bg-success me-1")
+        .text(function(col) { return col.Type })
+    events.append("span").attr("class", "badge bg-success")
+        .text(function(col) { return col.TargetAudience })
+
+    events.append("hr").attr("class", "col-12")
+
+    properties = events.append("ul").attr("class", "list-group list-group-flush")
+    properties.append("li").attr("class", "list-group-item")
+        .append("i").attr("class", "bi bi-calendar-range-fill").attr("title", "Date range")
+        .text(function(col)
+            {
+                var dateString = " "
+                if(col.StartDate){
+                    dateString += col.StartDate.toISOString().slice(0, 10);
+                    if(col.FinishDate){
+                        dateString += " - " + col.FinishDate.toISOString().slice(0, 10)
+                    }
+                }
+                return dateString
+
+            })
+    properties.append("li").attr("class", "list-group-item")
+        .append("i").attr("class", "bi bi-calendar-x-fill").attr("title", "Excluded days")
+        .text(function(col)
+            {
+                if(col.ExcludedDays){
+                    return " " + col.ExcludedDays
+                }
+                return ""
+            })
+
+    properties.append("li").attr("class", "list-group-item")
+        .append("i").attr("class", "bi bi-calendar-week-fill").attr("title", "Days")
+        .text(function(col)
+            {
+                if(col.Days){
+                    return " " + col.Days
+                }
+                return ""
+            })
+
+    properties.append("li").attr("class", "list-group-item")
+        .append("i").attr("class", "bi bi-clock-fill").attr("title", "Hour")
+        .text(function(col)
+            {
+                if(col.Hour){
+                    return " " + col.Hour
+                }
+                return ""
+            })
+
+    properties.append("li").attr("class", "list-group-item")
+        .append("i").attr("class", "bi bi-geo-alt-fill").attr("title", "Held in")
+        .text(function(col)
+            {
+                if(col.HeldIn){
+                    return " " + col.HeldIn
+                }
+                return ""
+            })
+
+    events.append("hr").attr("class", "col-12")
+
+    events.append("i").attr("class", "bi bi-currency-euro").attr("title", "Price")
+
+    events.append("small").attr("class", "text-muted")
+        .text(function(col) { return col.Price })
+
+    events.append("div").attr("class", "card-footer text-center")
+        .append("a").attr("class", "btn btn-primary")
+        .attr("href", function(col) { return col.URL }).text("Ver más")
+
 }
 
 function htmltable(json, config) {

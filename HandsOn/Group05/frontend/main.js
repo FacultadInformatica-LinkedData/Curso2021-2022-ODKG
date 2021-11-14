bbe62f09ba4d681bf9fbe74cb046b4ae66e748e9
AlexandreMorel <64785022+AlexandreMorel@users.noreply.github.com>
2021-11-14 22:59:56 +0100

    Sparql

--- HandsOn/Group05/frontend/main.js
+++ HandsOn/Group05/frontend/main.js
@@ -1,9 +1,9 @@
-
-
 function makeSPARQLQuery( endpointUrl, sparqlQuery, doneCallback ) {
 	var settings = {
-		headers: { Accept: 'application/sparql-results+json' },
-		data: { query: sparqlQuery }
+		// headers: { Accept: 'application/sparql-results+json',
+    // 'Access-Control-Allow-Origin': '*' }
+    headers: { Accept: 'application/sparql-results+json' },
+    data: {query: sparqlQuery}
 	};
 	return $.ajax( endpointUrl, settings ).then( doneCallback );
 }
@@ -13,17 +13,9 @@ $("#audience-select").on("change", update)
 
 
 
-var endpointUrl = 'https://query.wikidata.org/sparql',
-	sparqlQuery = "#Chats\n" +
-        "SELECT ?item ?itemLabel \n" +
-        "WHERE \n" +
-        "{\n" +
-        "  ?item wdt:P31 wd:Q146. # doit avoir comme nature chat\n" +
-        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". } # le label viendra de préférence dans votre langue, et autrement en anglais\n" +
-        "}";
 
 const district = $("#district-select").val()
-  
+
 
 const grid= new gridjs.Grid({
   columns: ['district'],
@@ -32,40 +24,71 @@ const grid= new gridjs.Grid({
   ]
 }).render(document.getElementById("wrapper"))
 
+var endpointUrl = 'http://localhost:9999/blazegraph/sparql',
+	sparqlQuery = "prefix : <http://groupfive.edu/kg/ontology/MadridEvents#> SELECT ?title ?description ?date ?price ?url ?address WHERE { ?facility :hasDistrict <http://groupfive.edu/kg/resources/district/Centro> . ?facility <http://www.w3.org/2006/vcard/ns#hasAddress> ?address. ?event :hasFacility ?facility .?event :hasTitle ?title . ?event :hasStartingDate ?date . ?event :hasEventUrl ?url . ?event <https://saref.etsi.org/core/hasPrice> ?price . }";
+  sparqlQuery2= " prefix : <http://groupfive.edu/kg/ontology/MadridEvents#> SELECT DISTINCT ?obj WHERE { ?district :hasDistrict ?obj }"
+
+makeSPARQLQuery( endpointUrl, sparqlQuery2, function( data ) {
+  $( 'body' ).append( $( '<pre>' ).text( JSON.stringify( data ) ) );
+  
+  var dt = []
+  console.log(data.results.bindings)
+  data.results.bindings.forEach(e => {dt.push(e.obj.value.split('/').splice(-1).join().split("%20").join(" "))})
+  console.log(dt)
+}
+);
+
+  makeSPARQLQuery( endpointUrl, sparqlQuery, function( data ) {
+		$( 'body' ).append( $( '<pre>' ).text( JSON.stringify( data ) ) );
+    
+    var dt = []
+    console.log(data.results.bindings[0].title)
+    data.results.bindings.forEach(e => {dt.push(e.title)})
+    console.log(district)
+    
+	}
+);
 function update(){
     // filter data based on selections
   const district = $("#district-select").val()
   const audience = $("#audience-select").val()
   const category = $("#category-select").val()
 
-  var endpointUrl = 'https://query.wikidata.org/sparql',
-	sparqlQuery = "#Chats\n" +
-        "SELECT ?item ?itemLabel \n" +
-        "WHERE \n" +
-        "{\n" +
-        "  ?item wdt:P31 wd:Q146. # doit avoir comme nature chat\n" +
-        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". } # le label viendra de préférence dans votre langue, et autrement en anglais\n" +
-        "}";
+  var endpointUrl = 'http://localhost:9999/blazegraph/sparql',
+	sparqlQuery = "prefix : <http://groupfive.edu/kg/ontology/MadridEvents#> SELECT ?title ?description ?date ?price ?url ?address WHERE { ?facility :hasDistrict <http://groupfive.edu/kg/resources/district/Centro> . ?facility <http://www.w3.org/2006/vcard/ns#hasAddress> ?address. ?event :hasFacility ?facility .?event :hasTitle ?title . ?event :hasStartingDate ?date . ?event :hasEventUrl ?url . ?event <https://saref.etsi.org/core/hasPrice> ?price . }";
+  console.log(sparqlQuery)
 
+
+  
+}
 
   makeSPARQLQuery( endpointUrl, sparqlQuery, function( data ) {
 		$( 'body' ).append( $( '<pre>' ).text( JSON.stringify( data ) ) );
     
     var dt = []
-    console.log(dt)
-    data.results.bindings.forEach(e => {dt.push(e.itemLabel)})
+    console.log(data)
+    data.results.bindings.forEach(e => {dt.push({"title":e.title.value, "price":e.price.value, "Address":e.address.value.toLowerCase(), "date":e.date.value.substr(0,10)}
+    )})
     console.log(dt)
     console.log(district)
+
 
     grid.updateConfig({
       // lets update the columns field only
       columns: [{
-        id:district,
-        name:district
+        id:"title",
+        name:"Title"
       },
       {
-        id:district,
-        name:district
+        id:"price",
+        name:"Price"
+      },
+      {
+        id:"Address",
+        name:"Address"
+      },{
+        id:"date",
+        name:"Date"
       }],
       pagination: {
         limit: 10
@@ -75,16 +98,10 @@ function update(){
       sort: true,
       data:dt
     }).forceRender();
-    
-
-    
-    
-		
 
 	}
 );
 
-}
 
 
 

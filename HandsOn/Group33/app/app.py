0463e9b8f3885014a2a10dc61b9fca191539c4b0
JeanBanzekulivakha <zhan.banzekulivakha@gmail.com>
2021-11-18 21:02:44 +0100

    FE part start

--- HandsOn/Group33/app/app.py
+++ HandsOn/Group33/app/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from openGraph import *
 
 app = Flask(__name__)
 obj = OpenGraph("mapped_data")
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
+    title = "E-scooter project"
+    return render_template("layouts/index.html", title=title)
+
+@app.route("/results/", methods=["GET"])
+def results():
+    title = "Results"
+    datalist = ["a", "2", "4"]
+    return render_template("layouts/results.html", title=title, datalist=datalist)
+
+@app.route("/sparql/predicates")
+def getSparqlPredicates():
     return jsonify(obj.allPredicats())
 
 @app.route('/sparql/community')
---
+++ HandsOn/Group33/app/templates/static/js/script.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+   function exampleFunction() {
+     return "Example"
+   }
+ 
+   $( "#exampleButton" ).click(function(){
+    exampleFunction();
+   });
+
+ });
+ 
